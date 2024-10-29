dec0ea3b42ab11eb1858be7b6f78d15bc5375939
Robin Hirst <147210082+RobinHirst11@users.noreply.github.com>
2024-10-29 11:24:50 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const grid = document.querySelector('.grid');
+const imageCount = 21;
+const imageIndices = Array.from({ length: imageCount }, (_, i) => i + 1);
 
-for (let i = 1; i <= 21; i++) {
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+shuffle(imageIndices);
+
+for (let i = 0; i < imageCount; i++) {
     const gridItem = document.createElement('div');
     gridItem.classList.add('grid-item');
     const img = document.createElement('img');
-    img.src = `images/${i}.svg`;
-    img.alt = `Image ${i}`;
+
+    const imageIndex = imageIndices[i];
+    img.src = `images/${imageIndex}.svg`;
+    img.alt = `Image ${imageIndex}`;
+
     gridItem.appendChild(img);
     grid.appendChild(gridItem);
 }
